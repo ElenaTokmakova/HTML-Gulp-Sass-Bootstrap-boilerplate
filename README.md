6a98c82b6a364d4e8ef04881679bc21bf485f6fa
Elena <elenatokmakova551@gmail.com>
2019-10-08 00:59:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Quick Start
+```
+# 1 Unzip the archive
+
+# 2 Navigate into the directory
+
+# 3 Install all node packages
+npm install
+
+# 4 Get started
+gulp serve - starts localhost server with browser-sync, watches HTML, Sass, JS with hot reloading
+gulp buil - minify CSS/JS and builds the app into the dist directory, ready for production
---
+++ gulpfile.js
@@ -0,0 +1,123 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      prefix = require('gulp-autoprefixer'),
+      postcss = require("gulp-postcss"),
+      autoprefixer = require("autoprefixer"),
+      sourcemaps = require("gulp-sourcemaps"),
+      concat = require('gulp-concat'),
+	  uglify = require('gulp-uglify'),
+      rename = require('gulp-rename'),
+      htmlreplace = require('gulp-html-replace'),
+      del = require('del'),
+      cssmin = require('gulp-cssmin'),
+      browserSync = require('browser-sync').create();
+
+const paths = {
+    styles: {
+        src: './assets/sass/main.scss',
+        dest: './assets/css',
+        dist: './dist/assets/css'
+    },
+    scripts: {
+        src: [
+            './assets/js/jquery-3.3.1.slim.min.js',
+            './assets/js/popper.min.js',
+            './assets/js/bootstrap.min.js',
+            './assets/js/scripts.js'
+        ],
+        dest: './assets/js',
+        dist: './dist/assets/js'
+    }
+};
+
+function style() {
+    return gulp
+        .src(paths.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream());
+}
+
+function script() {
+    return gulp
+        .src(paths.scripts.src)
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(browserSync.stream());
+}
+
+function minifyStyles() {
+    return gulp.src("assets/css/main.css")
+        .pipe(cssmin())
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest(paths.styles.dist));
+}
+
+function minifyScripts() {
+    return gulp.src("assets/js/main.js")
+        .pipe(uglify())
+        .pipe(rename('main.min.js'))
+        .pipe(gulp.dest(paths.scripts.dist));
+}
+
+function reload() {
+    browserSync.reload();
+}
+
+function serveAndWatch() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch(paths.styles.src, style);
+    gulp.watch(paths.scripts.src, script);
+    gulp.watch(['*.html', '*.php']).on('change', browserSync.reload);
+}
+
+async function clean() {
+    const result = del(['dist', 'assets/css/main.css*', 'assets/js/main*.js*']);
+    await Promise.resolve(result);
+}
+
+function buildAssets() {
+    return gulp.src([
+		'*.html',
+		'*.php',
+		'favicon.ico',
+		"assets/img/**"
+	], { base: './'})
+		.pipe(gulp.dest('dist'));
+}
+
+function renameSources() {
+    return gulp.src(['*.html', '**/*.php', '!dist', '!dist/**'])
+        .pipe(htmlreplace({
+            'js': 'assets/js/main.min.js',
+            'css': 'assets/css/main.min.css'
+        }))
+        .pipe(gulp.dest('dist/'));
+}
+
+exports.serveAndWatch = serveAndWatch;
+exports.style = style;
+exports.script = script;
+exports.minifyScripts = minifyScripts;
+exports.minifyStyles = minifyStyles;
+exports.clean = clean;
+
+const serve = gulp.series(style, script, serveAndWatch);
+
+const build = gulp.series(clean, style, script, minifyStyles, minifyScripts, buildAssets, renameSources);
+
+gulp.task('build', build);
+
+gulp.task('serve', serve);
+
+gulp.task('default', serve);
